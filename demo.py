51459c911bc0d101d54bc0eaab8f5079bbef227e
Zafar Takhirov <cc.rafaz@zafar.cc>
2016-08-14 19:02:25 -0700

    Added a tag, and attempt to use Arduino

--- demo.py
+++ demo.py
@@ -1,37 +1,29 @@
 #!/usr/bin/env python
 
 import fingerpi as fp
-
+from fingerpi.base import make_packet
 import time
+import serial
 # from fingerpi.fingerpi_base import command
 
 # from fingerpi.fingerpi_base import checksum as cs
 # from fingerpi.fingerpi_base import make_bytearray as mb
 # from fingerpi.fingerpi_base import printBytearray
 
+def byte2list(arr):
+    assert type(arr) == bytearray
+    res = []
+    for el in arr:
+        res.append(hex(el))
+    return res
+
+print byte2list(make_packet('comm', command = 'Open'))
+
+ser = serial.Serial(port = '/dev/cu.usbmodem1421', timeout = 2., baudrate = 9600)
+
+packet = make_packet('comm', command = 'CmosLed', parameter = 1)
+print ser.write(packet)
+print ser.read(12)
 
 
-f = fp.FingerPi('/dev/cu.usbmodem1421', baudrate = 9600)
 
-print f.Open(extra_info = True, check_baudrate = True)
-print f.ChangeBaudrate(57600)
-
-
-print f.CmosLed(True)
-time.sleep(1)
-print f.CmosLed(False)
-time.sleep(1)
-
-print f.GetEnrollCount()
-
-print f.ChangeBaudrate(9600)
-
-for idx in xrange(10):
-    print f.CmosLed(True)
-    time.sleep(1)
-    print f.CmosLed(False)
-    time.sleep(1)
-
-print f.sendCommand(command('CmosLed'), 0)
-
-print f.sendCommand(command('Close'))
