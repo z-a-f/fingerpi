d07abda59064c958c7ad26824efb6f0879b66f8e
Zafar Takhirov <cc.rafaz@zafar.cc>
2016-08-24 02:06:18 -0700

    First attempt in interactive menu

---
+++ fingerpi/interactive.py
@@ -0,0 +1,136 @@
+
+# Topmenu and the submenus are based of the example found at this location http://blog.skeltonnetworks.com/2010/03/python-curses-custom-menu/
+# The rest of the work was done by Matthew Bennett and he requests you keep these two mentions when you reuse the code :-)
+# Basic code refactoring by Andrew Scheller
+
+#
+
+## Usage: curses.wrapper(processmenu,menu_data) # menu_data is the menu data structure
+
+import curses
+
+## These should be the same as in the menu_data!!!
+# MENU = "menu"
+# COMMAND = "command"
+# EXITMENU = "exitmenu"
+
+## processrequest is defined in the `menu_data`
+from .menu_data import processrequest, MENU, COMMAND, EXITMENU
+
+# This function displays the appropriate menu and returns the option selected
+def runmenu(screen, menu, parent, status_mid = '', status_bottom = ''):
+    ## Need to somehow make these global
+    h = curses.color_pair(1) #h is the coloring for a highlighted menu option
+    n = curses.A_NORMAL #n is the coloring for a non highlighted menu option
+
+    mid_status_from_the_bottom = 3 # How manu rows from the bottom would a status region start?
+
+    # work out what text to display as the last menu option
+    if parent is None:
+        lastoption = "Exit"
+    else:
+        lastoption = "Return to %s menu" % parent['title']
+
+    optioncount = len(menu['options']) # how many options in this menu
+
+    pos=0 #pos is the zero-based index of the hightlighted menu option. Every time runmenu is called, position returns to 0, when runmenu ends the position is returned and tells the program what opt$
+    oldpos=None # used to prevent the screen being redrawn every time
+    x = None #control for while loop, let's you scroll through options until return key is pressed then returns pos to program
+
+    rows, cols = screen.getmaxyx()
+
+    # connection_status = 'Closed'
+    # response_status = ''
+
+    # Loop until return key is pressed
+    while x !=ord('\n'):
+        if pos != oldpos or x == curses.KEY_RESIZE: # In case window is resized, redraw everything!
+            if x == curses.KEY_RESIZE: 
+                screen.clear() # Clear borders and stuff!
+                rows, cols = screen.getmaxyx() # Update the rows and colms
+            else:
+                oldpos = pos
+
+            screen.border(0)
+            screen.addstr(2,2, menu['title'], curses.A_STANDOUT) # Title for this menu
+            screen.addstr(4,2, menu['subtitle'], curses.A_BOLD) #Subtitle for this menu
+
+            # Display all the menu items, showing the 'pos' item highlighted
+            for index in range(optioncount):
+                textstyle = n
+                if pos==index:
+                    textstyle = h
+                screen.addstr(5+index,4, "%d - %s" % (index+1, menu['options'][index]['title']), textstyle)
+            # Now display Exit/Return at bottom of menu
+            textstyle = n
+            if pos==optioncount:
+                textstyle = h
+            screen.addstr(5+optioncount,4, "%d - %s" % (optioncount+1, lastoption), textstyle)
+
+            # Add the status of the connection and of the response
+            if status_mid is not None:
+                screen.clrtobot()
+                screen.border(0) # Clear to bottom clears the borders as well :(
+                # Divide in multiple lines + skip 4 columns from both sides
+                idx = 0 # This makes sure that we don't go below the screen (if dividing the string into rows)
+                #while len(status_mid) > 0 and idx < mid_status_from_the_bottom:
+                screen.addstr(rows - mid_status_from_the_bottom + idx, 4, status_mid[:cols - 8])
+                    #status_mid = status_mid[cols-8:]
+                    #idx += 1
+
+            if status_bottom is not None:
+                screen.clrtoeol()
+                screen.border(0) # Clear to bottom clears the borders as well :(
+                screen.addstr(rows - 1, 4, 'Status: ' + status_bottom)
+            
+            #else:
+
+
+            screen.refresh()
+            # finished updating screen
+
+        x = screen.getch() # Gets user input
+
+        # What is user input?
+        if x >= ord('1') and x <= optioncount+1:#ord(str(optioncount+1)):
+            pos = x - ord('0') - 1 # convert keypress back to a number, then subtract 1 to get index
+        elif x == 258: # down arrow
+            if pos < optioncount:
+                pos += 1
+            else: pos = 0
+        elif x == 259: # up arrow
+            if pos > 0:
+                pos += -1
+            else: pos = optioncount
+        # elif x == curses.KEY_RESIZE:
+        #     # raise NotImplementedError('Resizing is not implemented yet - sorry, will get to it eventually!')
+        #     screen.refresh()
+        #     rows, cols = screen.getmaxyx()
+
+    # return index of the selected item
+    return pos
+
+# This function calls showmenu and then acts on the selected item
+def processmenu(screen, menu, parent=None, status_bottom = 'Uninitialized...'):
+    curses.init_pair(1,curses.COLOR_BLACK, curses.COLOR_WHITE)
+    curses.curs_set(0)
+    status_mid = ''
+    optioncount = len(menu['options'])
+    exitmenu = False
+    # response = None
+    while not exitmenu: #Loop until the user exits the menu
+        getin = runmenu(screen, menu, parent, status_mid, status_bottom)
+        if getin == optioncount:
+            exitmenu = True
+        elif menu['options'][getin]['type'] == COMMAND:
+            # screen.clear() #clears previous screen
+            status_mid, status_bottom = processrequest(menu['options'][getin])
+            ## Show the updated status
+            # screen.clear() #clears previous screen on key press and updates display based on pos
+        elif menu['options'][getin]['type'] == MENU:
+            screen.clear() #clears previous screen on key press and updates display based on pos
+            processmenu(screen, menu['options'][getin], menu, status_bottom) # display the submenu, and make sure the status is persistent
+            screen.clear() #clears previous screen on key press and updates display based on pos
+        elif menu['options'][getin]['type'] == EXITMENU:
+            exitmenu = True
+
---
+++ fingerpi/menu_data.py
@@ -0,0 +1,109 @@
+
+import fingerpi as fp
+
+MENU = "menu"
+COMMAND = "command"
+EXITMENU = "exitmenu"
+
+menu_data = {
+    'title': "GT-511C3 UART", 'type': MENU, 'subtitle': "Please select an option...",
+    'options':[
+        { 'title': "Initialize", 'type': COMMAND, 'command': 'C.Initialize()' },
+        { 'title': "Open", 'type': COMMAND, 'command': 'C.Open()' },
+        { 'title': "LED on/off", 'type': COMMAND, 'command': '' },
+        { 'title': "Enroll Sequence", 'type': COMMAND, 'command': '' },
+        { 'title': "All Commands", 'type': MENU, 'subtitle': "Please select an option...", 
+        'options': [
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Close", 'type': COMMAND, 'command': '' },
+            { 'title': "UsbInternalCheck", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+            { 'title': "Open", 'type': COMMAND, 'command': '' },
+        ]},
+    ]
+}
+
+class Commands():
+    ## Every method has to return `status` array of size 2
+    def __init__(self):
+        self.f = None
+        self.status = 'Uninitialized...'
+        self.open = False
+
+    def Initialize(self):
+        self.f = fp.FingerPi()
+        self.status = 'Closed'
+        return ['FingerPi() instance initialized...', self.status]
+    def Open(self):
+        ## Bottom status format:
+        # 'Status: Closed' or
+        # 'Status: Open\t`Baudrate`\t`firmware ver.`\t`device serial number`'
+        if self.f is None:
+            return ['Error: Please initialize the device first!', None]
+        self.open = True # Show the default status iff NOT initialized!
+        status = [None, None]
+        response = self.f.Open(extra_info = True, check_baudrate = True)
+        if response[0]['ACK']:
+            status[0] = ''
+            status[1] = 'Open\t%s\t%s\t%s'%(
+                response[0]['Parameter'],
+                response[1]['Data'][:4],
+                response[1]['Data'][8:]
+            )
+        return status
+
+## TODO: Commands in the menu_data?
+def processrequest(menu):
+    status = [None, None] # 0: Top, 1: Bottom
+    assert menu['type'] == COMMAND
+    # Check if the Commands object is created
+    try:
+        C
+    except:
+        C = Commands()
+    # Run the commands
+    try:
+        status = eval(menu['command'])
+        # We don't want to change the bottom status that often!
+        if C.open or status[1] == None:
+            status[1] = C.status
+    except Exception as e:
+        # e = sys.exc_info()
+        # raise e
+        # status = '\n\t'.join(map(str, e))
+        # status = 'Error: ' + str(e[1])
+        status = ['Error: (while running ' + menu['title'] + ') ' + str(e), C.status]
+
+    status = map(str, status)
+    return status
+
